7298d3f52ba4539134f84d86c22dc36afc7d31c4
peterbaumert <peterbaumert@users.noreply.github.com>
2020-11-21 21:43:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt install tmux git gawk net-tools coreutils
+apt install tmux git gawk net-tools coreutils bash-completion htop bwm-ng
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
