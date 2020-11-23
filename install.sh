58495aaabfbc43326b908b82aa5ae0e342fc8686
peterbaumert <peterbaumert@users.noreply.github.com>
2020-11-23 19:30:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux.conf -O ~/.tmux.co
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux/hostname.bash -O ~/.tmux/hostname.bash
 chmod +x ~/.tmux/hostname.bash
 
-mkdir -p /srv/bash/themes/agnoster-bash
-git clone https://github.com/speedenator/agnoster-bash.git /srv/bash/themes/agnoster-bash
+#mkdir -p /srv/bash/themes/agnoster-bash
+#git clone https://github.com/speedenator/agnoster-bash.git /srv/bash/themes/agnoster-bash
 
 wget https://github.com/peterbaumert/myLinux/raw/main/etc/profile.d/bash-profile.sh -O /etc/profile.d/bash-profile.sh
