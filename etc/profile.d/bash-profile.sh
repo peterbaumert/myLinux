732c3777fcc31c6bb0cb5ee6b33ab789c6fef07b
peterbaumert <peterbaumert@users.noreply.github.com>
2020-11-23 18:22:22 +0100

    Update bash-profile.sh

--- etc/profile.d/bash-profile.sh
+++ etc/profile.d/bash-profile.sh
@@ -4,4 +4,10 @@ alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'
 
-export PS1="\[\033[38;5;206m\]\u\[\033[38;5;15m\]@\[\033[38;5;2m\]$(hostname -f)\[\033[38;5;15m\]:\[\033[38;5;202m\]\w\[\033[38;5;15m\]\\$\[$(tput sgr0)\]"
+export THEME=/srv/bash/themes/agnoster-bash/agnoster.bash
+if [[ -f $THEME ]]; then
+    export DEFAULT_USER=root
+    source $THEME
+fi
+
+#export PS1="\[\033[38;5;206m\]\u\[\033[38;5;15m\]@\[\033[38;5;2m\]$(hostname -f)\[\033[38;5;15m\]:\[\033[38;5;202m\]\w\[\033[38;5;15m\]\\$\[$(tput sgr0)\]"
