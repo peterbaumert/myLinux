7e077bf1e19ada33de3cee4ca49e090b2fff029f
peterbaumert <peterbaumert@users.noreply.github.com>
2020-11-23 19:55:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-apt install tmux git gawk net-tools coreutils bash-completion htop bwm-ng wget
+apt install tmux git gawk net-tools coreutils bash-completion htop bwm-ng wget zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 mkdir -p ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux.conf -O ~/.tmux.conf
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux/hostname.bash -O ~/.tmux/hostname.bash
 chmod +x ~/.tmux/hostname.bash
-
-#mkdir -p /srv/bash/themes/agnoster-bash
-#git clone https://github.com/speedenator/agnoster-bash.git /srv/bash/themes/agnoster-bash
-
-wget https://github.com/peterbaumert/myLinux/raw/main/etc/profile.d/bash-profile.sh -O /etc/profile.d/bash-profile.sh
