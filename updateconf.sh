09a5bb5b08be414012c59a23cab009a68a8f930d
peterbaumert <peterbaumert@users.noreply.github.com>
2020-12-04 21:10:33 +0100

    Update updateconf.sh

--- updateconf.sh
+++ updateconf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.zshrc -O ~/.zshrc
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux.conf -O ~/.tmux.conf
+wget https://github.com/peterbaumert/myLinux/raw/main/~/.ssh/authorized_keys -O ~/.ssh/authorized_keys
