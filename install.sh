0e7b711cbe269cf1037261100dd6fc60aa4fa74c
peterbaumert <peterbaumert@users.noreply.github.com>
2020-12-04 21:07:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux.conf -O ~/.tmux.conf
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux/hostname.bash -O ~/.tmux/hostname.bash
 chmod +x ~/.tmux/hostname.bash
+
+wget https://github.com/peterbaumert/myLinux/raw/main/~/.ssh/authorized_keys -O ~/.ssh/authorized_keys
