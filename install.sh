bbbab116466412c5747c91d34d31eb2d2777f443
peterbaumert <peterbaumert@users.noreply.github.com>
2020-12-07 20:58:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 apt install tmux git gawk net-tools coreutils bash-completion htop bwm-ng wget zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wget https://github.com/peterbaumert/myLinux/raw/main/~/.zshrc -O ~/.zshrc
-
 mkdir -p ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux.conf -O ~/.tmux.conf
@@ -12,3 +9,6 @@ chmod +x ~/.tmux/hostname.bash
 
 mkdir -p ~/.ssh
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.ssh/authorized_keys -O ~/.ssh/authorized_keys
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget https://github.com/peterbaumert/myLinux/raw/main/~/.zshrc -O ~/.zshrc
