a9345fcd51efc0c6bbb371b87ad2a5989e87e396
peterbaumert <peterbaumert@users.noreply.github.com>
2021-03-15 18:44:02 +0100

    Update updateconf.sh

--- updateconf.sh
+++ updateconf.sh
@@ -2,3 +2,6 @@
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.zshrc -O ~/.zshrc
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux.conf -O ~/.tmux.conf
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.ssh/authorized_keys -O ~/.ssh/authorized_keys
+
+wget https://github.com/peterbaumert/myLinux/raw/main/etc/sysctl.d/10-custom-kernel-bbr.conf -O /etc/sysctl.d/10-custom-kernel-bbr.conf
+sysctl --system
