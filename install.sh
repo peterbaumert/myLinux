3f6fb11b184528e748ba84957cbbf2def60a0e78
peterbaumert <peterbaumert@users.noreply.github.com>
2021-03-15 18:43:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux.conf -O ~/.tmux.co
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.tmux/hostname.bash -O ~/.tmux/hostname.bash
 chmod +x ~/.tmux/hostname.bash
 
+wget https://github.com/peterbaumert/myLinux/raw/main/etc/sysctl.d/10-custom-kernel-bbr.conf -O /etc/sysctl.d/10-custom-kernel-bbr.conf
+sysctl --system
+
 mkdir -p ~/.ssh
 wget https://github.com/peterbaumert/myLinux/raw/main/~/.ssh/authorized_keys -O ~/.ssh/authorized_keys
 
